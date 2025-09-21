ceeade27ef4d1623351a3b6395ac90b03171a849
Amirhossein2006-hub <a.h.ayoubian@gmail.com>
2025-09-21 10:21:02 +0330

    rebueild in persian

---
+++ Online_Shop/Models/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Online_Shop.Models
+{
+    public class CartViewModel
+    {
+        public List<CartItem> cartItems { get; set; }
+        public decimal totalPrice { get; set; }
+
+        public CartViewModel()
+        {
+            cartItems = new List<CartItem>();
+        }
+    }
+}
