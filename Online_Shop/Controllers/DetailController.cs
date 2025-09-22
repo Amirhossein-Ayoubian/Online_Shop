8b437af53bb59c952cec075becd309610c741328
Amirhossein-Ayoubian <a.h.ayoubian@gmail.com>
2025-09-22 13:08:55 +0330

    Some Clean Code + Privacy

---
+++ Online_Shop/Controllers/DetailController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Shop.Data;
+using Online_Shop.Models;
+
+namespace Online_Shop.Controllers
+{
+    public class DetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ItemDetail(int id)
+        {
+            var item = Website.items.Find(p => p.product.id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        public IActionResult CategoryDetail(int id)
+        {
+            var category = Website.categories.Find(p => p.id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            List<Item> items = new List<Item>();
+            foreach (var item in Website.items)
+            {
+                if (item.product.category.id == id)
+                {
+                    items.Add(item);
+                }
+            }
+
+            CategoryItemViewModel viewModel = new CategoryItemViewModel()
+            {
+                category = category,
+                items = items
+            };
+
+            return View(viewModel);
+        }
+    }
+}
--- Online_Shop/Controllers/HomeController.cs
+++ Online_Shop/Controllers/HomeController.cs
@@ -34,43 +34,6 @@ namespace Online_Shop.Controllers
             return View(Website.items);
         }
 
-        public IActionResult Detail(int id)
-        {
-            var item = Website.items.Find(p => p.product.id == id);
-            if (item == null)
-            {
-                return NotFound();
-            }
-
-            return View(item);
-        }
-
-        public IActionResult CategoryDetail(int id)
-        {
-            var category = Website.categories.Find(p => p.id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            List<Item> items = new List<Item>();
-            foreach (var item in Website.items)
-            {
-                if (item.product.category.id == id)
-                {
-                    items.Add(item);
-                }
-            }
-
-            CategoryItemViewModel viewModel = new CategoryItemViewModel()
-            {
-                category = category,
-                items = items
-            };
-
-            return View(viewModel);
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
